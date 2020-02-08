88957c5153ba0757d9d11f2798556304bfcd2567
chenjiefei <nlpx_dc@hotmail.com>
2020-02-08 09:43:07 +0800

    draft jwt verify

--- jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/controller/AdministratorController.java
+++ jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/controller/AdministratorController.java
@@ -1,21 +1,18 @@
 package io.cjf.jcartadministrationback.controller;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
 import io.cjf.jcartadministrationback.constant.ClientExceptionConstant;
 import io.cjf.jcartadministrationback.dto.in.*;
 import io.cjf.jcartadministrationback.dto.out.AdministratorGetProfileOutDTO;
 import io.cjf.jcartadministrationback.dto.out.AdministratorListOutDTO;
+import io.cjf.jcartadministrationback.dto.out.AdministratorLoginOutDTO;
 import io.cjf.jcartadministrationback.dto.out.PageOutDTO;
 import io.cjf.jcartadministrationback.exception.ClientException;
 import io.cjf.jcartadministrationback.po.Administrator;
 import io.cjf.jcartadministrationback.service.AdministratorService;
+import io.cjf.jcartadministrationback.util.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Date;
 
 @RestController
 @RequestMapping("/administrator")
@@ -24,15 +21,12 @@ public class AdministratorController {
     @Autowired
     private AdministratorService administratorService;
 
-    @Value("${jwt.secret}")
-    private String jwtSecret;
-
-    @Value("${jwt.expire.duration}")
-    private Long jwtExpireDuration;
+    @Autowired
+    private JWTUtil jwtUtil;
 
     @GetMapping("/login")
-    public String login(@RequestParam String username,
-                        @RequestParam String password) throws ClientException {
+    public AdministratorLoginOutDTO login(@RequestParam String username,
+                                          @RequestParam String password) throws ClientException {
         Administrator administrator = administratorService.selectByUsername(username);
         if (administrator == null){
             throw new ClientException(ClientExceptionConstant.ADMINISTRATOR_USERNAME_NOT_EXIST_ERRCODE, ClientExceptionConstant.ADMINISTRATOR_USERNAME_NOT_EXIST_ERRMSG);
@@ -41,22 +35,8 @@ public class AdministratorController {
         BCrypt.Result result = BCrypt.verifyer().verify(password.toCharArray(), encPwdDB);
 
         if (result.verified) {
-
-            Date now = new Date();
-            long nowTimestamp = now.getTime();
-            long expireTimestamp = nowTimestamp + jwtExpireDuration;
-            Date expireTime = new Date(expireTimestamp);
-            Integer administratorId = administrator.getAdministratorId();
-
-            Algorithm algorithm = Algorithm.HMAC256(jwtSecret);
-            String token = JWT.create()
-                    .withIssuer("jcart")
-                    .withIssuedAt(now)
-                    .withSubject(username)
-                    .withClaim("administratorId", administratorId)
-                    .withExpiresAt(expireTime)
-                    .sign(algorithm);
-            return token;
+            AdministratorLoginOutDTO administratorLoginOutDTO = jwtUtil.issueToken(administrator);
+            return administratorLoginOutDTO;
         }else {
             throw new ClientException(ClientExceptionConstant.ADNINISTRATOR_PASSWORD_INVALID_ERRCODE, ClientExceptionConstant.ADNINISTRATOR_PASSWORD_INVALID_ERRMSG);
         }
