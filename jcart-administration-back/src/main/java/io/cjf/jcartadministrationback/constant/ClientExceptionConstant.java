5762da03da26a459721ff29aaf3697942561208e
chenjiefei <nlpx_dc@hotmail.com>
2020-02-15 17:24:31 +0800

    add image upload api

--- jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/constant/ClientExceptionConstant.java
+++ jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/constant/ClientExceptionConstant.java
@@ -8,6 +8,8 @@ public class ClientExceptionConstant {
     public static final String TOKEN_NOT_EXIST_ERRMSG = "token not exist";
     public static final String TOKEN_INVALID_ERRCODE = "0003";
     public static final String TOKEN_INVALID_ERRMSG = "token invalid";
+    public static final String IMAGE_INVALID_ERRCODE = "0004";
+    public static final String IMAGE_INVALID_ERRMSG = "image is not jpg or png";
 
     public static final String ADMINISTRATOR_USERNAME_NOT_EXIST_ERRCODE = "0101";
     public static final String ADMINISTRATOR_USERNAME_NOT_EXIST_ERRMSG = "administrator username not exist";
--- jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/controller/ImageController.java
+++ jcart-administration-back/src/main/java/io/cjf/jcartadministrationback/controller/ImageController.java
@@ -1,17 +1,42 @@
 package io.cjf.jcartadministrationback.controller;
 
+import io.cjf.jcartadministrationback.constant.ClientExceptionConstant;
+import io.cjf.jcartadministrationback.exception.ClientException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/image")
 public class ImageController {
 
+    private List<String> imageExts= Arrays.asList("jpg","png");
+
     @PostMapping("/upload")
-    public String upload(@RequestParam MultipartFile image){
-        return null;
+    public String upload(@RequestParam MultipartFile image) throws ClientException, IOException {
+        String originalFilename = image.getOriginalFilename();
+        String[] splits = originalFilename.split("\\.");
+        String ext = splits[splits.length - 1];
+        ext = ext.toLowerCase();
+        boolean contains = imageExts.contains(ext);
+        if (!contains){
+            throw new ClientException(ClientExceptionConstant.IMAGE_INVALID_ERRCODE, ClientExceptionConstant.IMAGE_INVALID_ERRMSG);
+        }
+        String uuid = UUID.randomUUID().toString();
+        String filename = String.format("%s.%s", uuid, ext);
+        String filepath = String.format("www/image/%s", filename);
+        try(FileOutputStream out = new FileOutputStream(filepath)){
+            byte[] data = image.getBytes();
+            out.write(data);
+        }
+        return filename;
     }
 }
